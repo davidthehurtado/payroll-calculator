2e6f8cf18fbd5cf3fcce59b11bf65c8b680894d8
David Hurtado <thedavidhurtado@gmail.com>
2025-09-25 13:16:38 -0500

    Modified code.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner;
+        myScanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = myScanner.nextLine();
+
+        System.out.print("Enter your hours worked: ");
+        double hoursWorked = Double.parseDouble(myScanner.nextLine());
+
+        System.out.print("Enter your pay rate: ");
+        double payRate = Double.parseDouble(myScanner.nextLine());
+
+        double grossPay = hoursWorked * payRate;
+
+        System.out.println("Payroll Summary for " + name);
+        System.out.printf("Gross Pay: $%.2f%n", grossPay);
+
+    }
 }
