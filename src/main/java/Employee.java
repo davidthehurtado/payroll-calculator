88fac8dd950174ea09672573dfd802515446de2a
David Hurtado <thedavidhurtado@gmail.com>
2025-10-07 16:01:15 -0500

    push for new assignment.

---
+++ src/main/java/Employee.java
@@ -0,0 +1,34 @@
+public class Employee {
+    private int employeeId;
+    private String name;
+    private double hoursWorked;
+    private double payRate;
+
+    public Employee(int employeeId, String name, double hoursWorked, double payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    // GROSS PAY
+    public double getGrossPay() {
+        return hoursWorked * payRate;
+    }
+}
--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -8,21 +8,32 @@ public class Main2 {
 
         try {
             FileReader fileReader = new FileReader("src/main/resources/employees.csv");
-            BufferedReader bufferedReader = new BufferedReader((fileReader));
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
 
             bufferedReader.readLine();
 
             String input;
+
             while ((input = bufferedReader.readLine()) != null) {
-                System.out.println();
 
+                String[] sections = input.split("\\|");
+
+                int id = Integer.parseInt(sections[0]);
+                String name = sections[1];
+                double hoursWorked = Double.parseDouble(sections[2]);
+                double payRate = Double.parseDouble(sections[3]);
+
+                Employee employee = new Employee(id, name, hoursWorked, payRate);
+
+                System.out.printf("Employee ID: %d | Name: %s | Gross Pay: $%.2f%n",
+                        employee.getEmployeeId(),
+                        employee.getName(),
+                        employee.getGrossPay());
             }
-
         } catch (FileNotFoundException e) {
-            System.out.println("Couldn't find the file: " + e);
+            System.out.println("Couldn't find the file: " + e.getMessage());
         } catch (IOException e) {
-            System.out.println("IO Exception: " + e);
+            System.out.println("IO Exception: " + e.getMessage());
         }
-
     }
 }
